b48a4b7f3af91021e48d07392a358af7b6ec4c47
pekkia95 <pekkia95@gmail.com>
2025-09-13 20:25:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+// SW minimale: niente precache e non intercetta media
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e => {
