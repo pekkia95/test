ef9784c51527cd8fd98dbf112b946bbdb0688f73
pekkia95 <pekkia95@gmail.com>
2025-09-13 19:00:31 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// SW minimale (non usato su iOS standalone)
-const CACHE = 'extratv-ios-min-v1';
+// SW minimale (non attivo su iOS standalone)
+const CACHE = 'extratvlive-ios-final-v1';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
